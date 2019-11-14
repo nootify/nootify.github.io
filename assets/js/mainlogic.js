8b124e0c1beffb354c0d29a5512562326b295f01
nootify <48371116+nootify@users.noreply.github.com>
2019-11-14 00:56:24 -0500

    fixed button event

--- assets/js/mainlogic.js
+++ assets/js/mainlogic.js
@@ -3,5 +3,12 @@ centerbutton.addEventListener("click", centerlogic);
 
 function centerlogic() {
     let centerdiv = document.getElementById("centerdiv");
-
+    
+    let centerdivState = centerdiv.style.display;
+    if(centerdivState == "block") {
+        centerdivState = "none";
+    }
+    else {
+        centerdivState = "block";
+    }
 }
