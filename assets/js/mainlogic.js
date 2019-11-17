4e619ad6ae0adb69656fa58e42c42e24871c9338
nootify <48371116+nootify@users.noreply.github.com>
2019-11-17 18:35:45 -0500

    Fix initial state

--- assets/js/mainlogic.js
+++ assets/js/mainlogic.js
@@ -1,21 +1,17 @@
-/*global document, console, alert */
-/* eslint no-console: "off" */
-(function () {
-    "use strict";
+"use strict";
 
-    function centerlogic() {
-        var centerdiv = document.getElementById("centerdiv");
+function centerlogic() {
+    var centerdiv = document.getElementById("centerdiv");
 
-        if (centerdiv.style.display === "block") {
-            centerdiv.style.display = "none";
-        } else {
-            centerdiv.style.display = "block";
-        }
+    if (centerdiv.style.display === "block") {
+        centerdiv.style.display = "none";
+    } else {
+        centerdiv.style.display = "block";
     }
+}
 
-    var centerbutton = document.getElementById("centerbutton");
-    document.addEventListener("DOMContentLoaded", centerlogic);
-    centerbutton.addEventListener("click", centerlogic);
+var centerbutton = document.getElementById("centerbutton");
+document.addEventListener("DOMContentLoaded", centerlogic);
+centerbutton.addEventListener("click", centerlogic);
 
-    // centerlogic();
-}());
+centerlogic();
