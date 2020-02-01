9753eea3bfe5e436805bfde5ec72766cb21c4b97
nootify <48371116+nootify@users.noreply.github.com>
2020-02-01 01:41:00 -0500

    Switched to jQuery front-end event handling

--- assets/js/mainlogic.js
+++ assets/js/mainlogic.js
@@ -1,20 +1,113 @@
-/* eslint-env browser */
+/* eslint-env browser, commonjs, es6, jquery */
+/*jslint
+    browser: true
+*/
+/*global
+    $, jQuery
+*/
+
 (function () {
     "use strict";
-
-    function dynamicUpdate() {
-        var dynamicText = document.getElementById("dynamictext");
-
-        if (dynamicText.style.display === "block") {
-            dynamicText.style.display = "none";
-        } else {
-            dynamicText.style.display = "block";
+    // SPOILERS!!! LOOK AWAY!!!
+    var text_list = ["This place is pretty empty.",
+                     "...",
+                     "I haven't really added much.",
+                     "...",
+                     "...but I figured it was better...",
+                     "...to make it less static-y.",
+                     "...",
+                     "...",
+                     "...",
+                     "Umm...",
+                     "Well, this is awkward.",
+                     "...",
+                     "...",
+                     "Stop clicking on that button.",
+                     "...",
+                     "Please...?",
+                     "...",
+                     "Pretty please...?",
+                     "...",
+                     "...",
+                     "Welp.",
+                     "...",
+                     "It was worth a shot.",
+                     "...",
+                     "...",
+                     "...",
+                     "Alright.",
+                     "...",
+                     "...",
+                     "...",
+                     "You're just going to<br>keep clicking away.",
+                     "Aren't ya?",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "Wow.",
+                     "You're REALLY determined.",
+                     "There isn't anything else to see.",
+                     "I promise.",
+                     "Really.",
+                     "Truly.",
+                     "It is <b>100%</b> the truth.",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "...",
+                     "I don't mean to sound rude, but...",
+                     "Don't you have anything better to do?",
+                     ""];
+    text_list = ["...",
+                     "I don't mean to sound rude, but...",
+                     "Don't you have anything better to do?",
+                     ""];
+    $(document).ready(function () {
+        // Helper function to play ending sequence
+        function end() {
+            $("#dynamictext").delay(4000).queue(function (next) {
+                $(this).html(text_list.shift());
+                next();
+            });
         }
-    }
-
-    var dynamicButton = document.getElementById("dynamicbutton");
-    document.addEventListener("DOMContentLoaded", dynamicUpdate);
-    dynamicButton.addEventListener("click", dynamicUpdate);
-
-    dynamicUpdate();
+        
+        $("#dynamicbutton").click(function () {
+            var $this = $(this),
+                essentials = 3,
+                sequence;
+            
+            if ($("#dynamictext").is(":hidden")) {
+                $("#dynamictext").show();
+            } else {
+                if (text_list.length > essentials) {
+                    $("#dynamictext").html(text_list.shift());
+                    
+                    // stops speedrunning by disabling the button
+                    $this.prop('disabled', true);
+                    setTimeout(function () {
+                        $this.prop('disabled', false);
+                    }, 1000);
+                } else {
+                    $("#dynamicbutton").hide();
+                    // Plays the ending sequence
+                    for (sequence = 0; sequence < essentials; sequence += 1) {
+                        end();
+                    }
+                }
+            }
+        });
+    });
 }());
